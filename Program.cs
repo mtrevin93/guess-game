0d4c7f92e19931a8c621f8c10fbe641e52ff1438
Michael Trevino <mtrevin93@gmail.com>
2021-08-24 16:05:45 -0400

    cheater mode added, all phases complete

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 
 
-
-// Phase 7
-// The program should be updated to...
-// 1. Prompt the user for a difficulty level before they are prompted to guess the number.
-// 2. The difficulty level should determine how many guesses the user gets.  
-//    The difficulty levels should be:
-//    • Easy - this gives the user eight guesses.
-//    • Medium - this gives the user six guesses.
-//    • Hard - this gives the user four guesses.
 // Phase 8
 // The program should be updated to...
 // 1. Add a difficulty level of "Cheater" which will cause the program to continue prompting the user until they get the answer correct.
 
 int count = 0;
+bool isCheater = false;
 
 Console.WriteLine("Pick a difficulty level...(Easy, Medium, Hard)");
 string difficulty = Console.ReadLine().ToLower();
@@ -27,8 +19,11 @@ if (difficulty == "medium"){
 if (difficulty == "hard"){
     count = 4;
 }
+if (difficulty == "cheater"){
+    isCheater = true;
+}
 
-Console.WriteLine("Guess the secret number...I'm not telling");
+Console.WriteLine($"{difficulty} mode! Guess the secret number...I'm not telling");
 
 string guess = "0";
 int secretNumber = new Random().Next(1,100);
@@ -38,17 +33,27 @@ while (int.Parse(guess) != secretNumber && count < 8){
     Console.WriteLine();
     if (int.Parse(guess) != secretNumber){
         Console.WriteLine($"Nope!");
-        if (count < 8)
-        count ++;
-        if (count == 8){
-            Console.WriteLine("Out of guesses! Too bad, so sad!");
-            break;
+        if (!isCheater){
+            if (count < 8)
+            count ++;
+            if (count == 8){
+                Console.WriteLine("Out of guesses! Too bad, so sad!");
+                break;
+            }
+            if (int.Parse(guess) < secretNumber){
+                Console.WriteLine($"{8 - count} Guesses left to guess the secret number...Try higer");
+            }
+            if (int.Parse(guess) > secretNumber){
+            Console.WriteLine($"{8 - count} Guesses left to guess the secret number...Try lower");
+            }
         }
-        if (int.Parse(guess) < secretNumber){
-            Console.WriteLine($"{8 - count} Guesses left to guess the secret number...Try higer");
-        }
-        if (int.Parse(guess) > secretNumber){
-        Console.WriteLine($"{8 - count} Guesses left to guess the secret number...Try lower");
+        if (isCheater){
+            if (int.Parse(guess) < secretNumber){
+                Console.WriteLine("Guess however many times you want, you cheat...Try higher");
+            }
+            if (int.Parse(guess) > secretNumber){
+            Console.WriteLine("Guesses however many times you want, you cheat...Try lower");
+            }
         }
     }
     else {
